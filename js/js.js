f8c5f2960cf31ba4ddad4285cda44d0c08fc3890
jwcm488 <jingweichai@gmail.com>
2014-01-17 21:55:09 -0500

    Put parallax scrolling, navigation bar click to section, and updated favicon.ico.

---
+++ js/js.js
@@ -0,0 +1,21 @@
+jQuery(document).ready(function ($) {
+    //initialise Stellar.js
+    $(window).stellar();
+    //Cache some variables
+    var links = $('nav').find('li');
+    slide = $('.slide');
+    mywindow = $(window);
+    htmlbody = $('html,body');
+    //Create a function that will be passed a slide number and then will scroll to that slide using jquerys animate. The Jquery easing plugin is also used, so we passed in the easing method of 'easeInOutQuint' which is available throught the plugin.
+    function goToByScroll(dataslide) {
+        htmlbody.animate({
+            scrollTop: $('.slide[data-slide="' + dataslide + '"]').offset().top -65
+        }, 2000, 'easeInOutQuint');
+    }
+    //When the user clicks on the navigation links, get the data-slide attribute value of the link and pass that variable to the goToByScroll function
+    links.click(function (e) {
+        e.preventDefault();
+        dataslide = $(this).attr('data-slide');
+        goToByScroll(dataslide);
+    });
+}); //Customized from Nettuts tutorial: http://webdesign.tutsplus.com/tutorials/complete-websites/create-a-parallax-scrolling-website-using-stellar-js/
